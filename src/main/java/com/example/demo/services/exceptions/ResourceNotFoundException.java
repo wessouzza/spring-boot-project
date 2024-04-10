7ecc74939a7ff7958773b2d0244e20e33a7c8ae5
wessouzza <wes-souza@hotmail.com>
2024-04-10 11:30:47 -0300

    Feat: Exceptions added

---
+++ src/main/java/com/example/demo/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.demo.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException(Object id){
+        super("Resource not found! ID: " + id);
+    }
+}
